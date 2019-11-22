b84f60c4bccd94b80e79df477dd840438dcbeaa5
Erick Surya Dinata <ericksuryadinata@gmail.com>
2019-11-22 16:38:50 +0700

    bug fix this is not constructor

--- src/Exceptions/RuntimeException.js
+++ src/Exceptions/RuntimeException.js
@@ -4,11 +4,11 @@ const NE = require('node-exceptions')
 
 class RuntimeException extends NE.RuntimeException {
   incompleteRequest (errorMessage) {
-    return new this('Incomplete or Error Occured while requesting with error message ' + errorMessage, 500, 'NP_INCOMPLETE_REQUEST')
+    return new RuntimeException('Incomplete or Error Occured while requesting with error message ' + errorMessage, 500, 'NP_INCOMPLETE_REQUEST')
   }
 
   incompleteBuild(errorMessage){
-    return new this('Incomplete or Error Occured while build with error message ' + errorMessage, 500, 'NP_INCOMPLETE_BUILD')
+    return new RuntimeException('Incomplete or Error Occured while build with error message ' + errorMessage, 500, 'NP_INCOMPLETE_BUILD')
   }
 }
 
